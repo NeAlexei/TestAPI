640adadad54094ae35ae52c98f057cb2c3d5db6d
NeAlexei <a.nechitaylo@hypervsn.com>
2019-02-12 13:10:22 +0300

    Initial commit

--- src/main/java/com/kinomo/config/Settings.java
+++ src/main/java/com/kinomo/config/Settings.java
@@ -2,7 +2,8 @@ package com.kinomo.config;
 
 public class Settings {
 
-    private static final String BASE_URL = "https://stage-platform.kino-mo.com";
+    public static final String BASE_URL = "https://stage-platform.kino-mo.com/api/v1/cct-projects";
     public static final String KM_AUTH = "km-auth";
-    public static final String SESSION_TOKEN = "";
+    public static final String SESSION_TOKEN = "cd74cc155b1ba732eab768f6d07a2551a6149f1a18a98c18268c4c240d52c46a5bd6f9779701d962b6808780";
+
 }
--- src/main/java/test/Test01.java
+++ src/main/java/test/Test01.java
@@ -3,16 +3,17 @@ import com.jayway.restassured.RestAssured;
 import org.apache.http.entity.ContentType;
 
 import static com.jayway.restassured.RestAssured.given;
+import static com.kinomo.config.Settings.*;
 
 public class Test01 {
 
     public String getAllTheLatestCCTProjects() {
         return given()
                 .log().all()
-                .header("km-auth", "cd74cc155b1ba732eab768f6d07a2551a6149f1a18a98c18268c4c240d52c46a5bd6f9779701d962b6808780")
+                .header(KM_AUTH, SESSION_TOKEN)
                 .header("Content-Type", "application/json")
                 .when()
-                .get("https://stage-platform.kino-mo.com" + "/api/v1/cct-projects")
+                .get(BASE_URL)
                 .then()
                 .assertThat()
                 .log().all()
