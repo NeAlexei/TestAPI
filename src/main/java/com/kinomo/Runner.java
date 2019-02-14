6b9e284dfa4744301ebc7adf7a568c8b116fdd03
NeAlexei <a.nechitaylo@hypervsn.com>
2019-02-14 18:53:51 +0300

    Add settings class

--- src/main/java/com/kinomo/Runner.java
+++ src/main/java/com/kinomo/Runner.java
@@ -1,14 +1,19 @@
 package com.kinomo;
 
 import test.Test01;
+import test.Test02;
 
 public class Runner {
 
     public static void main(String[] args) {
 
-        Test01 getAllTheLatestCCT = new Test01();
-        System.out.println(getAllTheLatestCCT.getAllTheLatestCCTProjects());
+//        Test01 getAllTheLatestCCT = new Test01();
+//        System.out.println(getAllTheLatestCCT.getAllTheLatestCCTProjects());
+
+        Test02 getAllPeople = new Test02();
+        String body = getAllPeople.getAllPeople();
+//        body.contains("");
+        System.out.println(body);
 
     }
-
 }
--- src/main/java/com/kinomo/config/Settings.java
+++ src/main/java/com/kinomo/config/Settings.java
@@ -4,6 +4,6 @@ public class Settings {
 
     public static final String BASE_URL = "https://stage-platform.kino-mo.com/api/v1/cct-projects";
     public static final String KM_AUTH = "km-auth";
-    public static final String SESSION_TOKEN = "cd74cc155b1ba732eab768f6d07a2551a6149f1a18a98c18268c4c240d52c46a5bd6f9779701d962b6808780";
+    public static final String SESSION_TOKEN = " ";
 
 }
