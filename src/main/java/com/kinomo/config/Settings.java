633537626f7715062fca145b19b97b6a0a6ccae9
NeAlexei <a.nechitaylo@hypervsn.com>
2019-02-20 16:38:56 +0300

    LC

--- src/main/java/com/kinomo/config/Settings.java
+++ src/main/java/com/kinomo/config/Settings.java
@@ -3,5 +3,5 @@ package com.kinomo.config;
 public class Settings {
     public static final String BASE_URL = "https://stage-platform.kino-mo.com/api/v1/cct-projects";
     public static final String KM_AUTH = "km-auth";
-    public static final String SESSION_TOKEN = "1eb4d461cf878becb0f3521211a1e5407e4ca8a5e990c31f237084edd5e20f9f5bd6f9779701d962b6808780";
+    public static final String SESSION_TOKEN = "";
 }
--- src/main/java/test/Test02.java
+++ src/main/java/test/Test02.java
@@ -13,6 +13,102 @@ public class Test02 { //POST - Save CCT ProjectContract
                 .then()
                 .statusCode(200)
                 .log().all();
+
+            String Json = "{\n" +
+                    "    \"scheme\": 0,\n" +
+                    "    \"projectName\": \"ProoojecT\",\n" +
+                    "    \"createdDate\": \"2019-02-05T14:34:46.233215Z\",\n" +
+                    "    \"projectPreview\": null,\n" +
+                    "    \"scenes\": [\n" +
+                    "        {\n" +
+                    "            \"position\": 0,\n" +
+                    "            \"name\": \"Scene 1\",\n" +
+                    "            \"data\": {\n" +
+                    "                \"imageActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"id\": \"blablabla\",\n" +
+                    "                        \"animationId\": \"anim_1\",\n" +
+                    "                        \"depth\": 0.3,\n" +
+                    "                        \"croppingSettings\": null\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"modelActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"id\": \"5c1bcab2e63f90774e8b88a8\",\n" +
+                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
+                    "                        \"animationId\": \"anim_1\"\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"templateActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"id\": \"2312\",\n" +
+                    "                        \"sourceId\": \"232\",\n" +
+                    "                        \"sourceType\": \"video\"\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"textActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"message\": \"test1\",\n" +
+                    "                        \"animationId\": \"anim_1\",\n" +
+                    "                        \"regionId\": \"default\",\n" +
+                    "                        \"fontId\": \"noto_sans\",\n" +
+                    "                        \"depth\": 0.3,\n" +
+                    "                        \"color\": \"#FFFFFF\"\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"priceActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"message\": \"Some Message\",\n" +
+                    "                        \"animationId\": \"AnimId\",\n" +
+                    "                        \"colorText\": \"colorText\",\n" +
+                    "                        \"colorTag\": \"colorTag\",\n" +
+                    "                        \"priceType\": 1\n" +
+                    "                    }\n" +
+                    "                ]\n" +
+                    "            }\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"position\": 1,\n" +
+                    "            \"name\": \"Scene 2\",\n" +
+                    "            \"data\": {\n" +
+                    "                \"imageActor\": [],\n" +
+                    "                \"modelActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"id\": \"5c1bcaa8e63f90774e8b889b\",\n" +
+                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
+                    "                        \"animationId\": \"anim_2\"\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"templateActor\": [],\n" +
+                    "                \"textActor\": [],\n" +
+                    "                \"priceActor\": null\n" +
+                    "            }\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"position\": 2,\n" +
+                    "            \"name\": \"Scene 3\",\n" +
+                    "            \"data\": {\n" +
+                    "                \"imageActor\": [],\n" +
+                    "                \"modelActor\": [\n" +
+                    "                    {\n" +
+                    "                        \"id\": \"5c1bb97ae63f90cc209390a8\",\n" +
+                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
+                    "                        \"animationId\": \"anim_3\"\n" +
+                    "                    }\n" +
+                    "                ],\n" +
+                    "                \"templateActor\": [],\n" +
+                    "                \"textActor\": [],\n" +
+                    "                \"priceActor\": null\n" +
+                    "            }\n" +
+                    "        }\n" +
+                    "    ],\n" +
+                    "    \"mediaSettings\": {\n" +
+                    "        \"width\": 880,\n" +
+                    "        \"height\": 880\n" +
+                    "    },\n" +
+                    "    \"softwareVersion\": \"18.0921.0\",\n" +
+                    "    \"projectVersion\": 2\n" +
+                    "}";
     }
 //                    .get("https://swapi.co/api/people/1")
 //                .then()
