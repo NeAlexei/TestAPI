d22826c13764e79d6714a4813358d9196959441b
NeAlexei <a.nechitaylo@hypervsn.com>
2019-02-21 12:58:54 +0300

    LC

--- src/main/java/com/kinomo/Runner.java
+++ src/main/java/com/kinomo/Runner.java
@@ -8,20 +8,11 @@ public class Runner {
 
     public static void main(String[] args) {
 
-        Test01 getAllTheLatestCCT = new Test01();
-        getAllTheLatestCCT.getAllTheLatestCCTVersions();
-//
-//        Test02 createProject = new Test02();
-//        createProject.createNewCCTProject();
+//        Test01 getAllTheLatestCCT = new Test01();
+//        getAllTheLatestCCT.getAllTheLatestCCTVersions();
 
-//        Test03 n = new Test03();
-//        n.verify();
-
-
-//        Test02 getAllPeople = new Test02();
-//        String body = getAllPeople.getAllPeople();
-////        body.contains("");
-//        System.out.println(body);
+        Test02 createProject = new Test02();
+        createProject.createNewCCTProject();
 
     }
 }
--- src/main/java/test/Test02.java
+++ src/main/java/test/Test02.java
@@ -2,120 +2,29 @@ package test;
 
 import static com.jayway.restassured.RestAssured.given;
 import static com.kinomo.config.Settings.*;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class Test02 { //POST - Save CCT ProjectContract
 
     public void createNewCCTProject() {
-            given()
+            given().log().all()
                 .header(KM_AUTH, SESSION_TOKEN)
                 .header("Content-Type", "application/json")
-                .get(BASE_URL)
-                .then()
-                .statusCode(200)
-                .log().all();
-
-            String Json = "{\n" +
-                    "    \"scheme\": 0,\n" +
-                    "    \"projectName\": \"ProoojecT\",\n" +
-                    "    \"createdDate\": \"2019-02-05T14:34:46.233215Z\",\n" +
-                    "    \"projectPreview\": null,\n" +
-                    "    \"scenes\": [\n" +
-                    "        {\n" +
-                    "            \"position\": 0,\n" +
-                    "            \"name\": \"Scene 1\",\n" +
-                    "            \"data\": {\n" +
-                    "                \"imageActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"id\": \"blablabla\",\n" +
-                    "                        \"animationId\": \"anim_1\",\n" +
-                    "                        \"depth\": 0.3,\n" +
-                    "                        \"croppingSettings\": null\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"modelActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"id\": \"5c1bcab2e63f90774e8b88a8\",\n" +
-                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
-                    "                        \"animationId\": \"anim_1\"\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"templateActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"id\": \"2312\",\n" +
-                    "                        \"sourceId\": \"232\",\n" +
-                    "                        \"sourceType\": \"video\"\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"textActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"message\": \"test1\",\n" +
-                    "                        \"animationId\": \"anim_1\",\n" +
-                    "                        \"regionId\": \"default\",\n" +
-                    "                        \"fontId\": \"noto_sans\",\n" +
-                    "                        \"depth\": 0.3,\n" +
-                    "                        \"color\": \"#FFFFFF\"\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"priceActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"message\": \"Some Message\",\n" +
-                    "                        \"animationId\": \"AnimId\",\n" +
-                    "                        \"colorText\": \"colorText\",\n" +
-                    "                        \"colorTag\": \"colorTag\",\n" +
-                    "                        \"priceType\": 1\n" +
-                    "                    }\n" +
-                    "                ]\n" +
-                    "            }\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "            \"position\": 1,\n" +
-                    "            \"name\": \"Scene 2\",\n" +
-                    "            \"data\": {\n" +
-                    "                \"imageActor\": [],\n" +
-                    "                \"modelActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"id\": \"5c1bcaa8e63f90774e8b889b\",\n" +
-                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
-                    "                        \"animationId\": \"anim_2\"\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"templateActor\": [],\n" +
-                    "                \"textActor\": [],\n" +
-                    "                \"priceActor\": null\n" +
-                    "            }\n" +
-                    "        },\n" +
-                    "        {\n" +
-                    "            \"position\": 2,\n" +
-                    "            \"name\": \"Scene 3\",\n" +
-                    "            \"data\": {\n" +
-                    "                \"imageActor\": [],\n" +
-                    "                \"modelActor\": [\n" +
-                    "                    {\n" +
-                    "                        \"id\": \"5c1bb97ae63f90cc209390a8\",\n" +
-                    "                        \"sourceType\": \"CUSTOM_MODEL\",\n" +
-                    "                        \"animationId\": \"anim_3\"\n" +
-                    "                    }\n" +
-                    "                ],\n" +
-                    "                \"templateActor\": [],\n" +
-                    "                \"textActor\": [],\n" +
-                    "                \"priceActor\": null\n" +
-                    "            }\n" +
-                    "        }\n" +
-                    "    ],\n" +
-                    "    \"mediaSettings\": {\n" +
-                    "        \"width\": 880,\n" +
-                    "        \"height\": 880\n" +
-                    "    },\n" +
-                    "    \"softwareVersion\": \"18.0921.0\",\n" +
-                    "    \"projectVersion\": 2\n" +
-                    "}";
+                .body(NewProjectJson)
+                .when()
+                .post(BASE_URL)
+                .then().log().all()
+                .statusCode(201)
+                .body("projectName", equalTo("01NewProject"));
     }
+
 //                    .get("https://swapi.co/api/people/1")
 //                .then()
 //                    .assertThat()
 //                    .statusCode(200)
 //                .and()
 //                .extract().body().asString();
+
     }
 
     /* Create new CCT Project:
